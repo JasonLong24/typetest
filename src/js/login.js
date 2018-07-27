15b4e703fc0ca008cdafa0eb714d17a7aefc1b4d
JasonLong24 <jasonlongball@gmail.com>
2018-07-26 20:27:35 -0400

    login with firebase

---
+++ src/js/login.js
@@ -0,0 +1,23 @@
+(function() {
+  var ui = new firebaseui.auth.AuthUI(firebase.auth());
+  var uiConfig = {
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        return true;
+      },
+      uiShown: function() {
+        document.getElementById('loader').style.display = 'none';
+      }
+    },
+    signInFlow: 'popup',
+    signInSuccessUrl: 'index.html',
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+    tosUrl: 'index.html',
+  };
+  ui.start('#firebaseui-auth-container', uiConfig);
+})()
